cda82defd8d0f40237439c2af660a5a691bc8a06
Kirtz Camron <kirtz.camron@gmail.com>
2026-03-23 00:30:51 -0400

    commit -m "Added index.html and script.js files for food data arrays and logging outputs."

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+const fruits = ["apple", "banana", "orange", "strawberry"];
+const vegetables = ["carrot", "broccoli", "spinach", "pepper"];
+const junkFood = ["chips", "candy", "soda", "pizza"];
+
+
+console.log("Fruits list:");
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
